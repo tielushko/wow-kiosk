83758b6b930c4f7a44670ce3c58407c5f963b824
dmorris6-hub <74027372+dmorris6-hub@users.noreply.github.com>
2021-02-24 16:03:27 -0500

    Revert "Layout Update 1.0"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react'
-import {Route} from 'react-router-dom'
-import Home from './pages/Home/Home'
+import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Route to='/' exact render={() => (
-        <Home />
-      )}/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
