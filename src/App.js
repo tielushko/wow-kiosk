2348ca48d01001113cf182f8d444d970296d6d60
Oleg Tielushko <tielushko@mail.usf.edu>
2021-03-25 13:27:34 -0400

    start out the handoff.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
-import React from 'react'
-import {Route} from 'react-router-dom'
-import Home from './pages/Home/Home'
-import './App.css';
-import Header from './components/Header';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import "./App.css";
+import Header from "./components/Header";
+import UserFetchField from "./components/UserFetchField";
+
+import Manatee from "./components/RouteTest/Manatee/Manatee";
+import Narwhal from "./components/RouteTest/Narwhal/Narwhal";
+import Whale from "./components/RouteTest/Whale/Whale";
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Route to='/' exact render={() => (
-        <Home />
-      )}/>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/manatee">Manatee</a>
+                    </li>
+                    <li>
+                        <a href="/narwhal">Narwhal</a>
+                    </li>
+                    <li>
+                        <a href="/whale">Whale</a>
+                    </li>
+                </ul>
+            </nav>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/whale">
+                        <Whale />
+                    </Route>
+                    <Route path="/manatee">
+                        <Manatee />
+                    </Route>
+                    <Route path="/narwhal">
+                        <Narwhal />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+            {/* <Route to="/" exact render={() => <Home />} />
+            <Route
+                to="/testing"
+                exact
+                render={() => {
+                    <UserFetchField />;
+                }}
+            /> */}
+        </div>
+    );
 }
 
 export default App;
--- src/components/UserFetchField.js
+++ src/components/UserFetchField.js
@@ -12,7 +12,7 @@ import {
     saveToken,
     createTable,
     getuserid,
-    tableservice
+    tableservice,
 } from "./TableFunctions";
 
 const videoSectionStyle = {
@@ -71,18 +71,21 @@ const startCall = async () => {
 
     const calleeInput = document.querySelector("#callee-input");
 
-    tableservice.retrieveEntity("KioskToken", calleeInput.value, "1", function(error, result, response) {
-    if (!error) {
-        const aUserToCall = result.Userid._;
-        call = callAgent.startCall(
-        [{ communicationUserId: aUserToCall }],
-        placeCallOptions
-        );
-    subscribeToRemoteParticipantInCall(call);
-             
-    }
-    });
-    
+    tableservice.retrieveEntity(
+        "KioskToken",
+        calleeInput.value,
+        "1",
+        function (error, result, response) {
+            if (!error) {
+                const aUserToCall = result.Userid._;
+                call = callAgent.startCall(
+                    [{ communicationUserId: aUserToCall }],
+                    placeCallOptions
+                );
+                subscribeToRemoteParticipantInCall(call);
+            }
+        }
+    );
 };
 
 const endCall = async () => {
@@ -174,90 +177,57 @@ async function localVideoView() {
     document.getElementById("local-feed-view").appendChild(view.target);
 }
 
-async function userlogin(username){
-
+async function userlogin(username) {
     const loginInput = document.querySelector("#Login-input");
-    tableservice.retrieveEntity("KioskToken", loginInput.value, "1", async function(error, result, response) {
-    if (!error) {
-    
-        const AcsUserID = result.Userid._;
-        const userDetailResponse = await refreshACSToken(AcsUserID);
-        console.log(userDetailResponse);
+    tableservice.retrieveEntity(
+        "KioskToken",
+        loginInput.value,
+        "1",
+        async function (error, result, response) {
+            if (!error) {
+                const AcsUserID = result.Userid._;
+                const userDetailResponse = await refreshACSToken(AcsUserID);
+                console.log(userDetailResponse);
 
-        //initializing the calling client and setting the token credential - creating callAgent.
-        const callClient = new CallClient();
-        const tokenCredential = new AzureCommunicationTokenCredential(
-            userDetailResponse.token
-        );
-        callAgent = await callClient.createCallAgent(tokenCredential, {
-            displayName: "Testing Callee",
-        });
-        //creating the local video stream to be sent with the application
-        deviceManager = await callClient.getDeviceManager();
+                //initializing the calling client and setting the token credential - creating callAgent.
+                const callClient = new CallClient();
+                const tokenCredential = new AzureCommunicationTokenCredential(
+                    userDetailResponse.token
+                );
+                callAgent = await callClient.createCallAgent(tokenCredential, {
+                    displayName: "Testing Callee",
+                });
+                //creating the local video stream to be sent with the application
+                deviceManager = await callClient.getDeviceManager();
 
-        // subscribing to an incoming call event -> fires whenever we are receiving an incoming call
-        callAgent.on("incomingCall", async (args) => {
-            const cameras = await deviceManager.getCameras();
-            const videoDeviceInfo = cameras[0];
-            localVideoStream = new LocalVideoStream(videoDeviceInfo);
-            localVideoView();
-            const addedCall = await args.incomingCall.accept({
-                videoOptions: { localVideoStreams: [localVideoStream] },
-            });
-            // accept the incoming call
-            call = addedCall;
+                // subscribing to an incoming call event -> fires whenever we are receiving an incoming call
+                callAgent.on("incomingCall", async (args) => {
+                    const cameras = await deviceManager.getCameras();
+                    const videoDeviceInfo = cameras[0];
+                    localVideoStream = new LocalVideoStream(videoDeviceInfo);
+                    localVideoView();
+                    const addedCall = await args.incomingCall.accept({
+                        videoOptions: { localVideoStreams: [localVideoStream] },
+                    });
+                    // accept the incoming call
+                    call = addedCall;
 
-            subscribeToRemoteParticipantInCall(addedCall);
-            // or reject the incoming call
-            // args.incomingCall.reject();
-        });
+                    subscribeToRemoteParticipantInCall(addedCall);
+                    // or reject the incoming call
+                    // args.incomingCall.reject();
+                });
 
-        callAgent.on("callsUpdated", (e) => {
-            e.removed.forEach((removedCall) => {
-                // dispose of video renders
-                localRenderer.dispose();
-                remoteRenderer.dispose();
-            });
-        });
-    }
-});
-    //const usertoken = getuserid(loginInput.value);
-    //console.log(usertoken);
-    //refreshACSToken(usertoken);
-
+                callAgent.on("callsUpdated", (e) => {
+                    e.removed.forEach((removedCall) => {
+                        // dispose of video renders
+                        localRenderer.dispose();
+                        remoteRenderer.dispose();
+                    });
+                });
+            }
+        }
+    );
 }
-/* comment for wilson - once the user hits the "Login" button, you will:
-            1. take the content of the input box near the login button 
-            2. Lookup the login name ex. "wilsonp" or "kiosk1" and pull up the ACS User ID that is associated with that user.
-            3. with that retrieved login name, we want to make a call to refresh the token for accessing the ACS.
-            4. call refreshACSToken(USER_ID_FOUND_IN_TABLE_HERE) -> see how i use fetch new user below in provision user.
-            5. debug with console. the token shoud be under userResponse.token
-            6. initialize the calling client, tokenCredential, call agent and etc, similar to what we have in provisionUser.
-        
-    Once the user types the correct login name in the box to call the user - "wilsonp" for example, we want to:
-            1. In the startCall button event handler we want to make a call to database to retrieve the actual ACS user ID from the login wilsonp
-            2. We want to place the call using that retrieved identity for ACS using the userToCall variable to store it.
-            3. what we have instead of this
-            
-            ...  blah blah...
-            const calleeInput = document.querySelector("#callee-input");
-            const userToCall = calleeInput.value;
-            call = callAgent.startCall(
-                [{ communicationUserId: userToCall }],
-                placeCallOptions
-            );
-            ...  blah blah...
-
-            we now have this! 
-            ...  blah blah...
-
-            const calleeInput = document.querySelector("#callee-input");
-            const userToCall = fetchACSIDFromTable(calleeInput.value); // table func that would return our user ACS ID
-            call = callAgent.startCall(
-                [{ communicationUserId: userToCall }],
-                placeCallOptions
-            ); 
-            */
 
 const UserFetchField = () => {
     const [userID, setUserID] = useState("UserID Here");
@@ -312,7 +282,7 @@ const UserFetchField = () => {
             <button onClick={userlogin} id="Login">
                 Log in
             </button>
-            <input type="text" id="Login-input"/>
+            <input type="text" id="Login-input" />
 
             <button onClick={provisionUser} id="provision-user-button">
                 Provision User
@@ -345,26 +315,12 @@ const UserFetchField = () => {
             </button>
 
             <div className="videoParent">
-                <section
-                    //style={videoSectionStyle}
-                    className="localVideoSectionStyle localVideo"
-                >
-                    <div
-                        id="local-feed-view"
-                        //style={videoStyle}
-                        className="videoStyle"
-                    ></div>
+                <section className="localVideoSectionStyle localVideo">
+                    <div id="local-feed-view" className="videoStyle"></div>
                 </section>
 
-                <section
-                    //style={videoSectionStyle}
-                    className="remoteVideoSectionStyle remoteVideo"
-                >
-                    <div
-                        id="remote-feed-view"
-                        //style={videoStyle}
-                        className="videoStyle"
-                    ></div>
+                <section className="remoteVideoSectionStyle remoteVideo">
+                    <div id="remote-feed-view" className="videoStyle"></div>
                 </section>
             </div>
         </React.Fragment>
