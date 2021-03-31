8c1327f28870c28aeb5d6803725c19c4468bfe37
Oleg Tielushko <tielushko@mail.usf.edu>
2021-03-31 16:23:38 -0400

    Reorganize items, create separate view fields for kiosk/volunteer/handoff.

--- src/App.js
+++ src/App.js
@@ -3,39 +3,35 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import "./App.css";
 import Header from "./components/Header";
-import UserFetchField from "./components/UserFetchField";
-
-import Manatee from "./components/RouteTest/Manatee/Manatee";
-import Narwhal from "./components/RouteTest/Narwhal/Narwhal";
-import Whale from "./components/RouteTest/Whale/Whale";
+// import UserFetchField from "./components/UserFetchField";
+import GroupCallHandoff from "./components/CallFields/GroupCallHandoff";
+import QRCodeForHandoff from "./components/CallFields/QRCodeForHandoff";
+import VolunteerView from "./components/CallFields/VolunteerView";
+// import KioskView from "./components/CallFields/KioskView.js";
 
 function App() {
     return (
         <div>
             <Header />
-            <nav>
+            {/* <nav>
                 <ul>
                     <li>
-                        <a href="/manatee">Manatee</a>
-                    </li>
-                    <li>
-                        <a href="/narwhal">Narwhal</a>
-                    </li>
-                    <li>
-                        <a href="/whale">Whale</a>
+                        <a href="/handoff">Handoff</a>
                     </li>
                 </ul>
-            </nav>
+            </nav> */}
             <BrowserRouter>
                 <Switch>
-                    <Route path="/whale">
-                        <Whale />
+                    <Route path="/handoff">
+                        <GroupCallHandoff />
                     </Route>
-                    <Route path="/manatee">
-                        <Manatee />
+                    <Route path="/kiosk">
+                        <QRCodeForHandoff />
+                        <Home />
                     </Route>
-                    <Route path="/narwhal">
-                        <Narwhal />
+                    <Route path="/volunteer">
+                        <QRCodeForHandoff />
+                        <VolunteerView />
                     </Route>
                 </Switch>
             </BrowserRouter>
---
+++ src/utilities/callingUtils.js
@@ -0,0 +1,217 @@
+import { fetchNewUser } from "./utils";
+import { tableservice } from "./TableFunctions";
+
+import {
+    CallClient,
+    LocalVideoStream,
+    Renderer,
+} from "@azure/communication-calling";
+import { AzureCommunicationTokenCredential } from "@azure/communication-common";
+import "../components/VideoChat.css";
+
+let call;
+let callAgent;
+let deviceManager;
+// let cameras;
+// let videoDeviceInfo;
+let placeCallOptions;
+//for rendering the video of local and the remote caller
+let localRenderer;
+let remoteRenderer;
+let remoteVideoStream;
+let localVideoStream;
+
+export {
+    call,
+    callAgent,
+    deviceManager,
+    placeCallOptions,
+    localRenderer,
+    remoteRenderer,
+    remoteVideoStream,
+    localVideoStream,
+};
+
+export function subscribeToRemoteParticipantInCall(callInstance) {
+    callInstance.remoteParticipants.forEach((p) => {
+        subscribeToRemoteParticipant(p);
+    });
+    callInstance.on("remoteParticipantsUpdated", (e) => {
+        e.added.forEach((p) => {
+            subscribeToRemoteParticipant(p);
+        });
+    });
+}
+
+export function subscribeToRemoteParticipant(remoteParticipant) {
+    remoteParticipant.videoStreams.forEach((v) => {
+        handleVideoStream(v);
+    });
+    remoteParticipant.on("videoStreamsUpdated", (e) => {
+        e.added.forEach((v) => {
+            handleVideoStream(v);
+        });
+    });
+}
+
+export function handleVideoStream(remoteVideoStream) {
+    remoteVideoStream.on("isAvailableChanged", async () => {
+        if (remoteVideoStream.isAvailable) {
+            remoteVideoView(remoteVideoStream);
+        } else {
+            remoteRenderer.dispose();
+        }
+    });
+    if (remoteVideoStream.isAvailable) {
+        remoteVideoView(remoteVideoStream);
+    }
+}
+
+export async function remoteVideoView(remoteVideoStream) {
+    remoteRenderer = new Renderer(remoteVideoStream);
+    const view = await remoteRenderer.createView();
+    document.getElementById("remote-feed-view").appendChild(view.target);
+}
+
+export async function localVideoView() {
+    localRenderer = new Renderer(localVideoStream);
+    const view = await localRenderer.createView();
+    document.getElementById("local-feed-view").appendChild(view.target);
+}
+
+export const startCall = async () => {
+    const cameras = await deviceManager.getCameras();
+    const videoDeviceInfo = cameras[0];
+    localVideoStream = new LocalVideoStream(videoDeviceInfo);
+    placeCallOptions = {
+        videoOptions: { localVideoStreams: [localVideoStream] },
+    };
+
+    localVideoView();
+
+    const calleeInput = document.querySelector("#callee-input");
+
+    tableservice.retrieveEntity(
+        "KioskToken",
+        calleeInput.value,
+        "1",
+        function (error, result, response) {
+            if (!error) {
+                const aUserToCall = result.Userid._;
+                call = callAgent.startCall(
+                    [{ communicationUserId: aUserToCall }],
+                    placeCallOptions
+                );
+                subscribeToRemoteParticipantInCall(call);
+            }
+        }
+    );
+};
+
+export const joinGroupCall = async () => {
+    const cameras = await deviceManager.getCameras();
+    const videoDeviceInfo = cameras[0];
+    localVideoStream = new LocalVideoStream(videoDeviceInfo);
+    placeCallOptions = {
+        videoOptions: { localVideoStreams: [localVideoStream] },
+    };
+
+    localVideoView();
+
+    // const groupCallInput = document.querySelector("#group-call-input");
+    // const groupToCall = { groupId: groupCallInput.value };
+    const groupToCall = { groupId: "c65edd6b-a68b-4434-b443-e36832261595" };
+    call = callAgent.join(groupToCall, placeCallOptions);
+
+    subscribeToRemoteParticipantInCall(call);
+};
+
+export const endCall = async () => {
+    // end the current call
+    localRenderer.dispose();
+    if (remoteRenderer) {
+        remoteRenderer.dispose();
+    }
+    await call.hangUp(/*{ forEveryone: true }*/);
+};
+export const provisionUser = async () => {
+    //fetching API object from the backend
+    const userDetailResponse = await fetchNewUser();
+    console.log(userDetailResponse);
+
+    //initializing the calling client and setting the token credential - creating callAgent.
+    const callClient = new CallClient();
+    const tokenCredential = new AzureCommunicationTokenCredential(
+        userDetailResponse.token
+    );
+    if (!callAgent) {
+        callAgent = await callClient.createCallAgent(tokenCredential, {
+            displayName: "Testing Callee",
+        });
+    }
+
+    //creating the local video stream to be sent with the application
+    deviceManager = await callClient.getDeviceManager();
+
+    // subscribing to an incoming call event -> fires whenever we are receiving an incoming call
+    callAgent.on("incomingCall", async (args) => {
+        const cameras = await deviceManager.getCameras();
+        const videoDeviceInfo = cameras[0];
+        localVideoStream = new LocalVideoStream(videoDeviceInfo);
+        localVideoView();
+        const addedCall = await args.incomingCall.accept({
+            videoOptions: { localVideoStreams: [localVideoStream] },
+        });
+        // accept the incoming call
+        call = addedCall;
+
+        subscribeToRemoteParticipantInCall(addedCall);
+        // or reject the incoming call
+        // args.incomingCall.reject();
+    });
+
+    callAgent.on("callsUpdated", (e) => {
+        e.removed.forEach((removedCall) => {
+            // dispose of video renders
+            localRenderer.dispose();
+            remoteRenderer.dispose();
+        });
+    });
+};
+
+export const refreshUser = async (userDetailResponse) => {
+    const callClient = new CallClient();
+    const tokenCredential = new AzureCommunicationTokenCredential(
+        userDetailResponse.token
+    );
+    callAgent = await callClient.createCallAgent(tokenCredential, {
+        displayName: "Testing Callee",
+    });
+    //creating the local video stream to be sent with the application
+    deviceManager = await callClient.getDeviceManager();
+
+    // subscribing to an incoming call event -> fires whenever we are receiving an incoming call
+    callAgent.on("incomingCall", async (args) => {
+        const cameras = await deviceManager.getCameras();
+        const videoDeviceInfo = cameras[0];
+        localVideoStream = new LocalVideoStream(videoDeviceInfo);
+        localVideoView();
+        const addedCall = await args.incomingCall.accept({
+            videoOptions: { localVideoStreams: [localVideoStream] },
+        });
+        // accept the incoming call
+        call = addedCall;
+
+        subscribeToRemoteParticipantInCall(addedCall);
+        // or reject the incoming call
+        // args.incomingCall.reject();
+    });
+
+    callAgent.on("callsUpdated", (e) => {
+        e.removed.forEach((removedCall) => {
+            // dispose of video renders
+            localRenderer.dispose();
+            remoteRenderer.dispose();
+        });
+    });
+};
